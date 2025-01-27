4684de84d25cdd03e3d15d9780946281b764fac1
Haki Ebibi <ebibihaki@gmail.com>
2025-01-27 08:38:49 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,15 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jobtrek.github.io/', // TODO: Change to your github url
-    base: 'progress-report', // TODO: Change to your repo name
+    site: 'https://HakiEbibi.github.io/', // TODO: Change to your github url
+    base: 'dossier-formation', // TODO: Change to your repo name
     integrations: [starlight({
         title: 'Progress  report',
         social: {
-            github: 'https://github.com/jobtrek/progress-report', // TODO: Change to your repo url
+            github: 'https://github.com/HakiEBIBI/dossier-formation', // TODO: Change to your repo url
         },
         editLink: {
-            baseUrl: 'https://github.com/jobtrek/progress-report/edit/main/docs/', // TODO: before /edit, change to your repo url
+            baseUrl: 'https://github.com/HakiEBIBI/dossier-formation/edit/main/docs/', // TODO: before /edit, change to your repo url
         },
         customCss: [
             // Relative path to your custom CSS file
